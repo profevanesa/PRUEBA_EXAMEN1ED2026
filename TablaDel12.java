ee618c4fde8f35ece5d3c7edb1f50c7e98a6e051
Vanesa Alfaro Gallego <vjag1255@gmail.com>
2026-01-13 16:24:06 +0100

    subo proyecto

---
+++ TablaDel12.java
@@ -0,0 +1,12 @@
+
+//fichero creado para examen ED
+public class TablaDel12
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 10; i++)
+        {
+            System.out.println("12 * "+ i + " = " + 12 * i + "\n");
+        }
+    }
+}
